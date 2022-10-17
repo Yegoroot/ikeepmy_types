e2da9fa5da53e4fa04f60e8f47c0121b3937512c
Yegor Kirsanov <easywayroot@gmail.com>
2022-10-17 03:40:23 +0300

    ikeepmy dist

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export * from './types/response';
+export * from './types/user';
---
+++ dist/types/user.js
@@ -0,0 +1,22 @@
+"use strict";
+// ----------------------------------------------------------
+// USER AND ROLES
+// ----------------------------------------------------------
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SignInFields = exports.SignUpFields = void 0;
+var SignUpFields;
+(function (SignUpFields) {
+    SignUpFields["name"] = "name";
+    SignUpFields["phone"] = "phone";
+    SignUpFields["password"] = "password";
+    SignUpFields["email"] = "email";
+    SignUpFields["code"] = "code";
+})(SignUpFields = exports.SignUpFields || (exports.SignUpFields = {}));
+// ----------------------------------------------------------
+// SIGIN
+// ----------------------------------------------------------
+var SignInFields;
+(function (SignInFields) {
+    SignInFields["phone"] = "phone";
+    SignInFields["password"] = "password";
+})(SignInFields = exports.SignInFields || (exports.SignInFields = {}));
