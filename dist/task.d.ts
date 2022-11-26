3d0cada09ebc07f49631c6fdd6f015fa78e70f1f
Yegor Kirsanov <easywayroot@gmail.com>
2022-11-26 14:55:14 +0300

    task interface

--- dist/task.d.ts
+++ dist/task.d.ts
@@ -6,22 +6,31 @@ declare enum TaskStatus {
     IN_WORK = "in_work",
     DONE = "done"
 }
+interface DeleveryService {
+    datetime: string;
+    coords: Coords;
+}
+interface WashingService {
+    datetime: string;
+    coords: Coords;
+    countCar: number;
+}
 interface ServicesMap {
-    [Roles.DELEVER]: {
-        str: string;
-    };
-    [Roles.WASHER]: {
-        str: string;
-    };
+    [Roles.DELEVER]: DeleveryService;
+    [Roles.WASHER]: WashingService;
 }
 export interface GenerateTask<T extends Roles.DELEVER | Roles.WASHER> {
     creator: User;
     responsable: User;
-    coords: Coords;
     datetime: string;
     status: TaskStatus;
     roleWorker: T;
     data: ServicesMap[T];
+    comments: {
+        user: User;
+        text: string;
+        datetime: string;
+    }[];
 }
 export declare type Task = GenerateTask<Roles.DELEVER> | GenerateTask<Roles.WASHER>;
 export {};
